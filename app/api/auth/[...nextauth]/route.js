6cde25bc76e3721dccfa553ae849cde6c8bc2a38
PARNITA-SINGH <singhparnita94@gmail.com>
2026-07-03 17:41:20 +0530

    connected to dynamodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { db } from "@/lib/dynamodb";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+
 
 const handler = NextAuth({
   providers: [
@@ -11,3 +14,11 @@ const handler = NextAuth({
 });
 
 export { handler as GET, handler as POST };
+
+export async function GET() {
+  const result = await db.send(new ScanCommand({
+    TableName: "Users",
+    Limit: 1,
+  }));
+  return Response.json({ success: true, result });
+}
