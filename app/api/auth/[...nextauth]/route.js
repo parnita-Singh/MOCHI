73a7a021467d89418bc7bfd69159804ad4d8cfa4
PARNITA-SINGH <singhparnita94@gmail.com>
2026-07-04 21:28:48 +0530

    add explicit nextauth secret

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,6 +8,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
